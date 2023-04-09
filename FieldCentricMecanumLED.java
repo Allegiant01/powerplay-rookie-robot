d8eabe88b6352bdcdeb8ad55a993a6dfd863c0e1
Allegiant01 <118638548+Allegiant01@users.noreply.github.com>
2023-04-09 17:06:31 -0400

    Add files via upload

---
+++ FieldCentricMecanumLED.java
@@ -0,0 +1,127 @@
+package org.firstinspires.ftc.teamcode;
+
+import static com.qualcomm.robotcore.hardware.DcMotor.RunMode.RUN_TO_POSITION;
+import static com.qualcomm.robotcore.hardware.DcMotor.RunMode.RUN_USING_ENCODER;
+import static com.qualcomm.robotcore.hardware.DcMotor.RunMode.STOP_AND_RESET_ENCODER;
+
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.hardware.rev.RevBlinkinLedDriver;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+
+@TeleOp
+public class FieldCentricMecanumLED extends LinearOpMode {
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+        RevBlinkinLedDriver coolLights;
+        double UNCLAMPED = 0.25, CLAMPED = 0;
+
+        DcMotor fLeftMotor = hardwareMap.get(DcMotor.class, "front_left_motor");
+        DcMotor fRightMotor = hardwareMap.get(DcMotor.class, "front_right_motor");
+        DcMotor bLeftMotor = hardwareMap.get(DcMotor.class, "back_left_motor");
+        DcMotor bRightMotor = hardwareMap.get(DcMotor.class, "back_right_motor");
+        Servo claw = hardwareMap.get(Servo.class, "clamp_servo");
+        DcMotor linearSlide = hardwareMap.get(DcMotor.class, "left_intake");
+        DcMotor linearSlide2 = hardwareMap.get(DcMotor.class, "right_intake");
+        Servo rotate = hardwareMap.get(Servo.class, "rotate_servo");
+        coolLights = hardwareMap.get(RevBlinkinLedDriver.class, "lights");
+
+        linearSlide.setDirection(DcMotor.Direction.REVERSE);
+        linearSlide2.setDirection(DcMotor.Direction.REVERSE);
+        fLeftMotor.setDirection(DcMotor.Direction.REVERSE);
+
+        linearSlide.setMode(STOP_AND_RESET_ENCODER);
+        linearSlide2.setMode(STOP_AND_RESET_ENCODER);
+        linearSlide.setMode(RUN_USING_ENCODER);
+        linearSlide2.setMode(RUN_USING_ENCODER);
+
+        BNO055IMU imu = hardwareMap.get(BNO055IMU.class, "imu");
+        BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
+        parameters.angleUnit = BNO055IMU.AngleUnit.DEGREES;
+        imu.initialize(parameters);
+
+        while(!imu.isGyroCalibrated() && !isStopRequested()) {
+            Thread.sleep(1);
+            telemetry.addData("Calibrating IMU...", imu.isGyroCalibrated());
+            telemetry.update();
+        }
+
+        telemetry.addData("IMU Calibrated", imu.isGyroCalibrated());
+        telemetry.update();
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
+        waitForStart();
+        while (opModeIsActive()) {
+            double rand = 5;
+            telemetry.addData("motor angle", rand);
+            double y = -gamepad1.left_stick_y;
+            double x = gamepad1.left_stick_x;
+            double turn = gamepad1.right_stick_x;
+            double direction = Math.atan2(y, x);
+            double magnitude = Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2));
+            double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(direction * magnitude), 1);
+            double fRightPower = (Math.sin(direction - (Math.PI)/4) * magnitude)/denominator;
+            double fLeftPower = (Math.sin(direction + (Math.PI)/4) * magnitude)/denominator;
+
+            fLeftMotor.setPower(fLeftPower);
+            bLeftMotor.setPower(fRightPower);
+            fRightMotor.setPower(fRightPower);
+            bRightMotor.setPower(fLeftPower);
+
+             if (gamepad1.right_stick_x != 0) {
+                    fLeftMotor.setPower(turn);
+                    bLeftMotor.setPower(turn);
+                    fRightMotor.setPower(-turn);
+                    bRightMotor.setPower(-turn);
+                }
+
+             //heights for linear slide are for medium junctions
+                if(gamepad1.left_bumper) {
+                    rotate.setPosition(0.75);
+                    linearSlide.setTargetPosition(1700);
+                    linearSlide2.setTargetPosition(1700);
+                    linearSlide2.setMode(RUN_TO_POSITION);
+                    linearSlide2.setPower(1);
+                    linearSlide.setMode(RUN_TO_POSITION);
+                    linearSlide.setPower(1);
+                    telemetry.addData("Left linear slide position", linearSlide.getCurrentPosition());
+                    telemetry.addData("Right linear slide position", linearSlide2.getCurrentPosition());
+                    telemetry.update();
+                    coolLights.setPattern(RevBlinkinLedDriver.BlinkinPattern.DARK_GREEN);
+                }
+                if(gamepad1.right_bumper) {
+                    claw.setPosition(UNCLAMPED);
+                    rotate.setPosition(1);
+                    linearSlide.setTargetPosition(0);
+                    linearSlide2.setTargetPosition(0);
+                    linearSlide2.setMode(RUN_TO_POSITION);
+                    linearSlide2.setPower(1);
+                    linearSlide.setMode(RUN_TO_POSITION);
+                    linearSlide.setPower(1);
+                    telemetry.addData("Left linear slide position", linearSlide.getCurrentPosition());
+                    telemetry.addData("Right linear slide position", linearSlide2.getCurrentPosition());
+                    telemetry.update();
+                    coolLights.setPattern(RevBlinkinLedDriver.BlinkinPattern.DARK_RED);
+                }
+
+                if(gamepad1.a) {
+                    claw.setPosition(UNCLAMPED);
+                }
+                if(gamepad1.y) {
+                    claw.setPosition(CLAMPED);
+                }
+                if(gamepad1.x) {
+                    rotate.setPosition(0.75);
+                }
+                if(gamepad1.b) {
+                    rotate.setPosition(1);
+                }
+        }
+    }
+}
